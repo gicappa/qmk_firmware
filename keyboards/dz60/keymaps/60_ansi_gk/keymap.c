3862940826cb667078a5a041995957d4c548c8fd
Gian Carlo Pace <giancarlo.pace@gmail.com>
2020-05-22 10:16:31 +0200

    fixing the layout

--- keyboards/dz60/keymaps/60_ansi_gk/keymap.c
+++ keyboards/dz60/keymaps/60_ansi_gk/keymap.c
@@ -3,13 +3,16 @@
 // helpers
 #define _______ KC_TRNS
 
+// config
+#define TAPPING_TOGGLE 2
+
 // layers
 #define BASE 0
 #define FNKC 1
 #define CURS 2
 #define MISC 3
 
-// keys
+// layer selectors
 #define _L1_SPC LT(FNKC, KC_SPC)
 #define _L2_A LT(CURS, KC_A)
 #define _L3_FN TT(MISC)
@@ -42,8 +45,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	LAYOUT_60_ansi(
 		KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-		KC_CAPS, _L2_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,  KC_J,    KC_K,    KC_L,      KC_SCLN, KC_QUOT, KC_ENT,
-		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,    KC_N,    KC_M,      KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
+		KC_CAPS, _L2_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
 		KC_LCTL, KC_LALT, KC_LGUI,                   _L1_SPC,                            KC_RGUI, KC_RALT, KC_RCTL, _L3_FN
     ),
 
@@ -64,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 		_______, _______, _______,                   _______,                            _______, _______, _______, _______
     ),
 
@@ -85,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______, _______, _______,
 		_______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_UP,   KC_END,  _______, _______, _______, _______,
 		_______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______,
-		_______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_INS,  KC_PGUP, _______,          _______,
+		_______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_INS,  KC_PGUP, _______, _______,
 		_______, _______, _______,                   _______,                            _______, _______, _______, _______
     ),
 
@@ -108,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 		_______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-		_______, _______, _______, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, _______, _______,  _______,  _______,          _______,
+		_______, _______, _______, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, _______, _______, _______, _______, _______,
 		_______, _______, _______,                   _______,                            _______, _______, _______, _______
     ),
 
