c7581abd461d02c484806eb6a759f49ece66beae
Gian Carlo Pace <giancarlo.pace@gmail.com>
2020-05-25 09:12:24 +0200

    Solving the issue of a confusion of tap mod misbehaving

---
+++ keyboards/dz60/keymaps/60_ansi_gk/config.h
@@ -0,0 +1,10 @@
+#include "config_common.h"
+
+//#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 200
+
+#undef TAPPING_TOGGLE
+#define TAPPING_TOGGLE 2
