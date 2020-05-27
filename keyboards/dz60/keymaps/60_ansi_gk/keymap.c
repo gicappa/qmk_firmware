495bd28a7953548ba3eac0609e2f5b42ba57b831
Gian Carlo Pace <giancarlo.pace@gmail.com>
2020-05-27 17:27:05 +0200

    mapping the command on g and h

--- keyboards/dz60/keymaps/60_ansi_gk/keymap.c
+++ keyboards/dz60/keymaps/60_ansi_gk/keymap.c
@@ -21,8 +21,8 @@
 #define _K MT(MOD_RALT, KC_K)
 #define _S MT(MOD_LCTL, KC_S)
 #define _L MT(MOD_RCTL, KC_L)
-#define _V MT(MOD_LGUI, KC_V)
-#define _M MT(MOD_RGUI, KC_M)
+#define _G MT(MOD_LGUI, KC_G)
+#define _H MT(MOD_RGUI, KC_H)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |----------------------------------------------------------------------------------------------------+
      * | Tab    |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |   [  |   ]  |   \   |
      * |----------------------------------------------------------------------------------------------------+
-     * | Caps    |A^CURS|S\ctl |D\alt |F\sft |   G  |   H  |J\Rsft|K\Ralt|L\Rctl|   ;  |   '  |    Enter    |
+     * | Caps    |A^CURS|S\ctl |D\alt |F\sft |G\Cmd |H\RCmd|J\Rsft|K\Ralt|L\Rctl|   ;  |   '  |    Enter    |
      * |----------------------------------------------------------------------------------------------------+
      * | Shift     |   Z  |   X  |   C  | V\Cmd|   B  |   N  |M\RCmd|   ,  |   .  |   /  |      RShift      |
      * |----------------------------------------------------------------------------------------------------+
@@ -42,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	LAYOUT_60_ansi(
 		KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-		KC_CAPS, _L2_A,   _S,      _D,      _F,      KC_G,    KC_H,    _J,      _K,      _L,      KC_SCLN, KC_QUOT, KC_ENT,
-		KC_LSFT, KC_Z,    KC_X,    KC_C,    _V,      KC_B,    KC_N,    _M,      KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+		KC_CAPS, _L2_A,   _S,      _D,      _F,      _G,      _H,      _J,      _K,      _L,      KC_SCLN, KC_QUOT, KC_ENT,
+		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
 		KC_LCTL, KC_LALT, KC_LGUI,                   _L1_SPC,                            KC_RGUI, KC_RALT, KC_RCTL, _L3_FN
     ),
 
