6b0275a275e4d09960972f7d497e76ddead25329
Gian Carlo Pace <giancarlo.pace@gmail.com>
2020-07-29 19:05:09 +0200

    fixing a copy paste error and renaming layers according to the actual position

--- keyboards/dz60/keymaps/60_ansi_gk/keymap.c
+++ keyboards/dz60/keymaps/60_ansi_gk/keymap.c
@@ -13,10 +13,10 @@
 // layer selectors
 #define _L1_Q   LT(FNKC, KC_Q)
 #define _L1_P   LT(FNKC, KC_P)
-#define _L1_Z   LT(CODE, KC_Z)
-#define _L1_SLA LT(FNKC, KC_SLASH)
-#define _L2_A   LT(ARRW, KC_A)
-#define _L3_FN  TT(MEDI)
+#define _L2_Z   LT(CODE, KC_Z)
+#define _L2_SLA LT(CODE, KC_SLASH)
+#define _L3_A   LT(ARRW, KC_A)
+#define _L4_FN  TT(MEDI)
 
 //tap-mod
 #define _F MT(MOD_LSFT, KC_F)
@@ -49,9 +49,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	LAYOUT_60_ansi(
 		KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
 		KC_TAB,  _L1_Q,   KC_W,    _E,      _R,      KC_T,    KC_Y,    _U,      _I,      KC_O,    _L1_P,   KC_LBRC, KC_RBRC, KC_BSLS,
-		KC_CAPS, _L2_A,   _S,      _D,      _F,      KC_G,    KC_H,    _J,      _K,      _L,      KC_SCLN, KC_QUOT, KC_ENT,
-		XXXXXXX, _L1_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  _L1_SLA, XXXXXXX,
-		XXXXXXX, XXXXXXX, XXXXXXX,                   _SPC,                               XXXXXXX, XXXXXXX, XXXXXXX, _L3_FN
+		KC_CAPS, _L3_A,   _S,      _D,      _F,      KC_G,    KC_H,    _J,      _K,      _L,      KC_SCLN, KC_QUOT, KC_ENT,
+		XXXXXXX, _L2_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  _L2_SLA, XXXXXXX,
+		XXXXXXX, XXXXXXX, XXXXXXX,                   _SPC,                               XXXXXXX, XXXXXXX, XXXXXXX, _L4_FN
     ),
 
    /* Functions Layer
