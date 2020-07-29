b4a7031daf768129e5c51a75faf3b24de78cca43
Gian Carlo Pace <giancarlo.pace@gmail.com>
2020-07-29 19:01:28 +0200

    I forgot to activare the layer

--- keyboards/dz60/keymaps/60_ansi_gk/keymap.c
+++ keyboards/dz60/keymaps/60_ansi_gk/keymap.c
@@ -48,9 +48,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 	LAYOUT_60_ansi(
 		KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
-		KC_TAB,  _L1_Q,    KC_W,   _E,      _R,      KC_T,    KC_Y,    _U,      _I,      KC_O,    _L1_P,   KC_LBRC, KC_RBRC, KC_BSLS,
+		KC_TAB,  _L1_Q,   KC_W,    _E,      _R,      KC_T,    KC_Y,    _U,      _I,      KC_O,    _L1_P,   KC_LBRC, KC_RBRC, KC_BSLS,
 		KC_CAPS, _L2_A,   _S,      _D,      _F,      KC_G,    KC_H,    _J,      _K,      _L,      KC_SCLN, KC_QUOT, KC_ENT,
-		XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
+		XXXXXXX, _L1_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  _L1_SLA, XXXXXXX,
 		XXXXXXX, XXXXXXX, XXXXXXX,                   _SPC,                               XXXXXXX, XXXXXXX, XXXXXXX, _L3_FN
     ),
 
